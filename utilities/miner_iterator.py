d80f965c3eb7c59db04ac539191e6674afa49a9d
Sid <sid.data.universe@gmail.com>
2024-03-21 21:38:40 -0700

    Start iterator at 200 for fresh start.

--- utilities/miner_iterator.py
+++ utilities/miner_iterator.py
@@ -17,7 +17,8 @@ class MinerIterator:
         self.miner_uids = sorted(copy.deepcopy(miner_uids))
         # Start the index at a random position. This helps ensure that miners with high UIDs aren't penalized if
         # the validator restarts frequently.
-        self.index = random.randint(0, len(self.miner_uids) - 1)
+        # Temporarily hard code to start at 200 to more quickly restart on the relevant models.
+        self.index = 200
         self.lock = threading.Lock()
 
     def __iter__(self):
