a421e88ef5ac32b0e4f0031f5c245c549500e6a3
Sid <sid.data.universe@gmail.com>
2024-08-06 14:33:21 -0700

    Define the pretraining CompetitionId IntEnum.

---
+++ competitions/data.py
@@ -0,0 +1,16 @@
+from enum import IntEnum
+
+
+class CompetitionId(IntEnum):
+    """Unique identifiers for each competition."""
+
+    B7_MODEL = 0
+
+    M772_MODEL = 1
+
+    B3_MODEL = 2
+
+    # Overwrite the default __repr__, which doesn't work with
+    # bt.logging for some unknown reason.
+    def __repr__(self) -> str:
+        return f"{self.value}"
